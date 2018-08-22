3e0f82b299a505747df5a14e405e7b84e3294876
Alex Kerney <abk@mac.com>
2018-08-22 11:50:17 -0400

    Pulling about page content from contentservice.gmri.org

--- src/pages/mariner-about/mariner-about.ts
+++ src/pages/mariner-about/mariner-about.ts
@@ -1,25 +1,37 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MarinerAboutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { AppConfig } from '../../providers/appconfig/appconfig';
 
-@IonicPage()
+
+@IonicPage({
+  name: 'AboutPage'
+})
 @Component({
   selector: 'page-mariner-about',
   templateUrl: 'mariner-about.html',
 })
 export class MarinerAboutPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  aboutContent: string
+
+  constructor(
+    public appConfig: AppConfig,
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MarinerAboutPage');
+    this.initializeDrupalContent()
   }
 
+
+  initializeDrupalContent() {
+    for ( var cKey in this.appConfig.drupalContent ) {
+      if ( this.appConfig.drupalContent[cKey].field_neracoos_buoy_pwa_id[0].value == 'about_page_content') {
+        this.aboutContent = this.appConfig.drupalContent[cKey].body[0].value
+      }
+    }
+  }
 }
