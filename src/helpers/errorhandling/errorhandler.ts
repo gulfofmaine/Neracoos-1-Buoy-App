194a76376791bbd234e79f6754c50558d6d01c4a
Alex Kerney <abk@mac.com>
2018-08-28 10:52:33 -0400

    Add Sentry exception tracking

--- src/helpers/errorhandling/errorhandler.ts
+++ src/helpers/errorhandling/errorhandler.ts
@@ -1,5 +1,9 @@
+// import Raven = require('raven-js')
+import Raven from 'raven-js'
 import { AlertController, IonicErrorHandler } from 'ionic-angular'
 import { Inject, Injectable } from '@angular/core'
+
+Raven.config('https://eab04522f42c4efab9d5bfe7d8594e9c@sentry.io/1270344').install()
 
 @Injectable()
 export class GMRIErrorHandler extends IonicErrorHandler {
@@ -7,10 +11,11 @@ export class GMRIErrorHandler extends IonicErrorHandler {
         @Inject(AlertController) private alerts: AlertController
     ) {
         super()
-        console.log('Hello from error handler')
     }
 
     handleError(error: any): void {
+        Raven.captureException(error.origionalError || error)
+        
         const alert = this.alerts.create({
             title: 'An Error Has Occured',
             subTitle: 'That did not go as we planned.',
