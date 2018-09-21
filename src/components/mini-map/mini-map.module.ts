af7c496e13445b7a7ecea4f0aed19eb60ffff2d6
Alex Kerney <abk@mac.com>
2018-09-21 09:42:18 -0400

    folder cleanup

--- src/components/mini-map/mini-map.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MiniMapComponent } from './mini-map';
-@NgModule({
-	declarations: [MiniMapComponent],
-	imports: [],
-	exports: [MiniMapComponent]
-})
-export class MiniMapComponentModule {}
