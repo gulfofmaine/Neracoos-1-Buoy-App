e7c18127d1bd3774ad288502db07be10262be53a
Alex Kerney <abk@mac.com>
2019-01-02 12:00:21 -0500

    Platform selector component allowing similar components that depend on having a single platform

--- src/Features/ERDDAP/Platform/Info/index.spec.tsx
+++ src/Features/ERDDAP/Platform/Info/index.spec.tsx
@@ -1,63 +1,39 @@
-import { mount } from 'enzyme'
-import * as React from 'react'
+import { mount } from "enzyme"
+import * as React from "react"
 
-import { PlatformFeatureWithDatasets } from '../../types'
-import {
-    ErddapPlatformInfoPanelBase,
-    Props,
-    ReduxProps
-} from './index'
+import { PlatformFeatureWithDatasets } from "../../types"
+import { ErddapPlatformInfoPanel } from "./index"
 
-
-function setup(platforms: PlatformFeatureWithDatasets[], platformId: string) {
-    const props: Props & ReduxProps = {
-        platformId,
-        platforms
+describe("ErddapPlatformInfoPanel", () => {
+  it("Should display the platform info", () => {
+    const platform: PlatformFeatureWithDatasets = {
+      geometry: {
+        coordinates: [-65.9, 42.3],
+        type: "Point"
+      },
+      id: "N01",
+      properties: {
+        attribution: [
+          {
+            attribution: "Data management by NERACOOS",
+            program: {
+              name: "NERACOOS",
+              website: "http://neracoos.org"
+            }
+          }
+        ],
+        mooring_site_desc: "NorthEast Channel",
+        readings: []
+      },
+      type: "Feature"
     }
 
-    const enzymeWrapper = mount(<ErddapPlatformInfoPanelBase {...props} />)
+    const enzymeWrapper = mount(<ErddapPlatformInfoPanel platform={platform} />)
 
-    return {
-        enzymeWrapper,
-        props
-    }
-}
-
-
-describe('ErddapPlatformInfoPanel', () => {
-    it("It should not explode when there isn't a matching plaform", () => {
-        const { enzymeWrapper } = setup([], 'N01')
-
-        expect(enzymeWrapper.text()).toBeNull()
-    })
-
-    it('Should display the platform info', () => {
-        const platform: PlatformFeatureWithDatasets = {
-            geometry: {
-                coordinates: [-65.9, 42.3],
-                type: 'Point'
-            },
-            id: 'N01',
-            properties: {
-                attribution: [{
-                    attribution: 'Data management by NERACOOS',
-                    program: {
-                        name: 'NERACOOS',
-                        website: 'http://neracoos.org'
-                    }
-                }],
-                mooring_site_desc: 'NorthEast Channel',
-                readings: []
-            },
-            type: 'Feature'
-        }
-
-        const { enzymeWrapper } = setup([platform], 'N01')
-
-        expect(enzymeWrapper.find('.card-title').text()).toContain(platform.id)
-        expect(enzymeWrapper.find('.card-text').text()).toContain(platform.properties.mooring_site_desc)
-        expect(enzymeWrapper.find('.card-text').text()).toContain(platform.geometry.coordinates[0])
-        expect(enzymeWrapper.find('.card-text').text()).toContain(platform.geometry.coordinates[1])
-        expect(enzymeWrapper.find('.card-text').text()).toContain(platform.properties.attribution[0].attribution)
-    })
+    expect(enzymeWrapper.find(".card-title").text()).toContain(platform.id)
+    expect(enzymeWrapper.find(".card-text").text()).toContain(platform.properties.mooring_site_desc)
+    expect(enzymeWrapper.find(".card-text").text()).toContain(platform.geometry.coordinates[0])
+    expect(enzymeWrapper.find(".card-text").text()).toContain(platform.geometry.coordinates[1])
+    expect(enzymeWrapper.find(".card-text").text()).toContain(platform.properties.attribution[0].attribution)
+  })
 })
--- src/Features/ERDDAP/Platform/Info/index.tsx
+++ src/Features/ERDDAP/Platform/Info/index.tsx
@@ -1,78 +1,36 @@
 /**
  * Platform info panel
  */
-import { Geometry } from '@turf/helpers'
-import * as React from 'react'
-import { connect } from 'react-redux'
-import {
-    Card,
-    CardBody,
-    CardText,
-    CardTitle
-} from 'reactstrap'
+import { Geometry } from "@turf/helpers"
+import * as React from "react"
+import { Card, CardBody, CardText, CardTitle } from "reactstrap"
 
-import { StoreState } from '@app/constants'
-import { round } from '@app/Shared/math'
+import { round } from "@app/Shared/math"
 
-import { PlatformFeatureWithDatasets } from '../../types'
-
-
-export interface Props {
-    /** selected platform id */
-    platformId: string
-}
-
-export interface ReduxProps {
-    platforms: PlatformFeatureWithDatasets[]
-}
-
-function mapStateToProps({ erddap }: StoreState): ReduxProps {
-    return {
-        platforms: erddap.platforms
-    }
-}
-
+import { RenderProps } from "../Grabber"
 
 /**
  * Platform info panel
+ * @param platform
  */
-export class ErddapPlatformInfoPanelBase extends React.Component<Props & ReduxProps, object> {
-    
-    public render() {
-        if ( this.props.platforms.length > 0) {
-            const platforms = this.props.platforms.filter((p) => p.id as string === this.props.platformId)
-
-            if (platforms.length > 0) {
-                const platform = platforms[0]
-
-                if (platform.properties) {
-                    const { id } = platform
-                    const { attribution, mooring_site_desc } = platform.properties
-                    const { coordinates } = platform.geometry as Geometry
-
-                    return (
-                        <Card>
-                            <CardBody>
-                                <CardTitle>Buoy { id }</CardTitle>
-                                <CardText>
-                                    { mooring_site_desc }
-                                    <br />
-                                    <b>Lat:</b> { round(coordinates[1] as number)} <b>Lon:</b> {round(coordinates[0] as number)}<br />
-                                    { attribution.map((attr, key) => (
-                                        <React.Fragment key={key}><a href={attr.program.website}>{ attr.attribution }</a><br /></React.Fragment>
-                                    ))}
-                                </CardText>
-                            </CardBody>
-                        </Card>
-                    )
-                }
-            }
-        }
-
-        // return null if nothing else can be displayed
-        return null
-    }
-}
-
-/** Redux connected version of Platform Info. See [[ErddapPlatformInfoPanelBase]] for details. */
-export const ErddapPlatformInfoPanel = connect(mapStateToProps)(ErddapPlatformInfoPanelBase)
+export const ErddapPlatformInfoPanel: React.SFC<RenderProps> = ({ platform }) => (
+  <Card>
+    <CardBody>
+      <CardTitle>Buoy {platform.id}</CardTitle>
+      <CardText>
+        {platform.properties.mooring_site_desc}
+        <br />
+        <b>Lat:</b>
+        {round((platform.geometry as Geometry).coordinates[1] as number)} <b>Lon:</b>{" "}
+        {round((platform.geometry as Geometry).coordinates[0] as number)}
+        <br />
+        {platform.properties.attribution.map((attr, key) => (
+          <React.Fragment key={key}>
+            <a href={attr.program.website}>{attr.attribution}</a>
+            <br />
+          </React.Fragment>
+        ))}
+      </CardText>
+    </CardBody>
+  </Card>
+)
