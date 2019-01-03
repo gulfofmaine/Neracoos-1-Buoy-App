961416b60f1975c40bc8e1c2dc375d3ed54beeea
Alex Kerney <abk@mac.com>
2019-01-03 17:08:23 -0500

    Add Erddap wind conditions

---
+++ src/Features/ERDDAP/Platform/Observations/WindCondition/index.tsx
@@ -0,0 +1,78 @@
+/**
+ * Wind Observed conditions component
+ */
+import * as Sentry from "@sentry/browser"
+import * as React from "react"
+import sizeMe, { SizeMeProps } from "react-sizeme"
+import { Alert, Col, Row } from "reactstrap"
+
+import { WindTimeSeriesChart } from "@app/components/Charts"
+import { DataTimeSeries } from "@app/Shared/timeSeries"
+
+import { PlatformFeatureWithDatasets } from "../../../types"
+import { ErddapDatasetLoader } from "../../Dataset/Loading"
+import { ErddapDatasetStatus } from "../../Dataset/Status"
+
+interface Props {
+  platform: PlatformFeatureWithDatasets
+}
+
+const windStandards = new Set(["wind_from_direction", "wind_speed", "wind_speed_of_gust"])
+
+export const ErddapWindObservedConditionBase: React.SFC<Props & SizeMeProps> = ({ platform, size }) => {
+  // adjust number of barbs based on width
+  let barbsPerDay = 5
+  if (size.width < 800) {
+    barbsPerDay = 2
+  }
+
+  const windDatasets = platform.properties.readings.filter(reading =>
+    windStandards.has(reading.data_type.standard_name)
+  )
+
+  const windTimeSeries: DataTimeSeries[] = windDatasets.map(reading => ({
+    name: reading.data_type.long_name,
+    timeSeries: reading.readings,
+    unit: reading.data_type.units
+  }))
+
+  Sentry.addBreadcrumb({
+    category: "ERDDAP Wind",
+    data: {
+      platformId: platform.id as string,
+      windDatasets
+    }
+  })
+
+  if (windDatasets.length === 0) {
+    Sentry.captureMessage(("Unable to display wind data for " + platform.id) as string)
+    return noWind
+  }
+
+  return (
+    <React.Fragment>
+      <ErddapDatasetStatus datasets={windDatasets} />
+      <ErddapDatasetLoader platformId={platform.id as string} datasets={windDatasets}>
+        <React.Fragment>
+          {windTimeSeries[0].timeSeries !== undefined && windTimeSeries[0].timeSeries.length > 0 ? (
+            <Row>
+              <Col>
+                <WindTimeSeriesChart days={7} data={windTimeSeries} barbsPerDay={barbsPerDay} legend={true} />
+              </Col>
+            </Row>
+          ) : null}
+        </React.Fragment>
+      </ErddapDatasetLoader>
+    </React.Fragment>
+  )
+}
+
+const noWind = (
+  <Row>
+    <Col>
+      <Alert color="warning">Unable to display wind info</Alert>
+    </Col>
+  </Row>
+)
+
+export const ErddapWindObservedCondition = sizeMe()(ErddapWindObservedConditionBase)
--- src/components/Charts/WindTimeSeries.tsx
+++ src/components/Charts/WindTimeSeries.tsx
@@ -2,178 +2,158 @@
  * Wind time series chart component that can display steady and gust speeds
  * in addition to wind barbs for wind direction
  */
-import Highcharts from 'highcharts'
-import addWindBarbModule from 'highcharts/modules/windbarb'
-import * as React from 'react'
+import Highcharts from "highcharts"
+import addWindBarbModule from "highcharts/modules/windbarb"
+import * as React from "react"
 import {
-    Chart,
-    HighchartsChart,
-    Legend,
-    SplineSeries,
-    Tooltip,
-    WindBarbSeries,
-    withHighcharts,
-    XAxis,
-    YAxis,
-} from 'react-jsx-highcharts'
+  Chart,
+  HighchartsChart,
+  Legend,
+  SplineSeries,
+  Tooltip,
+  WindBarbSeries,
+  withHighcharts,
+  XAxis,
+  YAxis
+} from "react-jsx-highcharts"
 
-import { round } from '@app/Shared/math'
-import { DataTimeSeries } from '@app/Shared/timeSeries'
-import { 
-    conversion, 
-    convertUnit 
-} from '@app/Shared/unitConversion'
-
+import { round } from "@app/Shared/math"
+import { DataTimeSeries } from "@app/Shared/timeSeries"
+import { conversion, convertUnit } from "@app/Shared/unitConversion"
 
 /**
  * Allow our tooltip to convert windspeeds to units that people might be more used to.
- * 
+ *
  * @param this Highcharts position value
  */
 function pointFormatter(this: any) {
-    return `${(new Date(this.x).toLocaleString())}<br />` + this.points.map((p) => {
-        if (p.series.name === 'Direction') {
-            return `<b>${p.series.name}:</b> ${Math.round(p.point.direction)} (${p.point.beaufort})`
+  return (
+    `${new Date(this.x).toLocaleString()}<br />` +
+    this.points
+      .map(p => {
+        if (p.series.name === "Direction") {
+          return `<b>${p.series.name}:</b> ${Math.round(p.point.direction)} (${p.point.beaufort})`
         }
-        return `<b>${p.series.name}:</b> ${p.y} knots ${convertUnit('knot', p.y)}`
-    }).join('<br />')
+        return `<b>${p.series.name}:</b> ${p.y} knots ${convertUnit("knot", p.y)}`
+      })
+      .join("<br />")
+  )
 }
 
-
 const plotOptions = {
-    time: {
-    	useUTC: false
-    }
+  time: {
+    useUTC: false
+  }
 }
 
 interface Props {
-    /** Wind data to display */
-    data: DataTimeSeries[]
-    /** Number of days back to filter dataset by */
-    days: number
-    /** Hight to try to limit chart to */
-    height: number
-    /** Should the chart show a legend */
-    legend: boolean
-    /** Home many wind barbs should the chart show for each day */
-    barbsPerDay: number
+  /** Wind data to display */
+  data: DataTimeSeries[]
+  /** Number of days back to filter dataset by */
+  days: number
+  /** Hight to try to limit chart to */
+  height: number
+  /** Should the chart show a legend */
+  legend: boolean
+  /** Home many wind barbs should the chart show for each day */
+  barbsPerDay: number
 }
 
 addWindBarbModule(Highcharts)
-
 
 /**
  * Wind time series chart component that can display steady and gust speeds
  * in addition to wind barbs for wind direction
  */
 export class WindTimeSeriesChartBase extends React.Component<Props, object> {
-    constructor(props: Props) {
-        super(props)
+  constructor(props: Props) {
+    super(props)
+  }
+
+  public render() {
+    // Extract wind direction from windspeed data
+    const speeds = this.props.data.filter(d => !d.name.toLowerCase().includes("direction"))
+    const directions = this.props.data.filter(d => d.name.toLowerCase().includes("direction"))
+
+    const daysAgo = new Date()
+    daysAgo.setDate(daysAgo.getDate() - this.props.days)
+
+    const filteredSpeeds = speeds.map((d, index) => {
+      const data = d.timeSeries.filter(r => r.time > daysAgo).map(r => [r.time.valueOf(), round(r.reading, 1)])
+      return data
+    })
+
+    if (filteredSpeeds[0].length < 1) {
+      return null
     }
-
-    public render() {
-        // Extract wind direction from windspeed data
-        const speeds = this.props.data.filter((d) => ! d.name.includes('direction'))
-        const directions = this.props.data.filter((d) => d.name.includes('direction'))
-
-        const daysAgo = new Date()
-        daysAgo.setDate(daysAgo.getDate() - this.props.days)
-
-        const filteredSpeeds = speeds.map((d, index) => {
-            const data = d.timeSeries.filter(
-                (r) => r.time > daysAgo
-            ).map(
-                (r) => [r.time.valueOf(), round(r.reading, 1)]
-            )
-            return data
-        })
-
-        if (filteredSpeeds[0].length < 1) {
-            return null
-        }
-        const speedsSeries = speeds.map((d, index) => {
-            // Filter windspeeds to only include data from the time range
-            const data = d.timeSeries.filter(
-                (reading) => reading.time > daysAgo
-            ).map( // Return Highcharts Spline dataformat [date, reading]
-                (r) => [r.time.valueOf(), conversion(r.reading, 'm/s', 'knot') ]
-            )
-
-            const nameParts = d.name.split('_')
-            const name = nameParts[nameParts.length - 1]
-            const nameCaps = name[0].toUpperCase() + name.slice(1)
-
-            return (
-                <SplineSeries
-                    key={index}
-                    name={nameCaps}
-                    marker={{enabled: false}}
-                    data={data} />
-            )
-        })
-
-        /** Array of wind data. Highcharts uses [time, speed, direction] for each reading */
-        const windData: number[][] = []
-
-        /** Make sure our speed and direction data actually exists */
-        if (speeds.length > 0 && directions.length > 0 ) {
-            const speed = speeds[0]
-            const direction = directions[0]
-
-            let speedTs = speed.timeSeries.filter((r) => r.time > daysAgo)
-            let directionTs = direction.timeSeries.filter((r) => r.time > daysAgo)
-
-            // cross filter our time series so that we only have matching times
-            const directionTimes = directionTs.map((r) => r.time.toISOString())
-            speedTs = speedTs.filter((r) => directionTimes.filter((d) => d === r.time.toISOString()).length > 0)
-            const speedTimes = speedTs.map((r) => r.time.toISOString())
-            directionTs = directionTs.filter((r) => speedTimes.filter((d) => d === r.time.toISOString()).length > 0)
-
-            if ( speedTs.length === directionTs.length) {
-                // Figure out how many samples should be skipped between readings to manage chart density
-                const stride = Math.round(speedTs.length / (this.props.barbsPerDay * this.props.days))
-
-                for (let index = 0; index <= speedTs.length; index += stride) {
-                    if (speedTs[index] !== undefined && directionTs[index] !== undefined) {
-                        windData.push([
-                            directionTs[index].time.valueOf(),
-                            round(speedTs[index].reading, 1),
-                            directionTs[index].reading
-                        ])
-                    }
-                }
-            }
-        }
-
-        return (
-            <HighchartsChart time={plotOptions.time}>
-                <Chart height={this.props.height} />
-
-                <XAxis type='datetime' />
-
-                <YAxis
-                    softMin={0}>
-                    <YAxis.Title>Knots</YAxis.Title>
-                    { speedsSeries }
-
-                    { windData.length > 0 ? (
-                        <WindBarbSeries
-                            name='Direction'
-                            color='red'
-                            data={windData} />
-                    ) : ( null )}
-                </YAxis>
-
-                { this.props.legend ? (
-                    <Legend />
-                ) : null}
-
-                <Tooltip 
-                    formatter={pointFormatter}
-                    shared={true} />
-            </HighchartsChart>
+    const speedsSeries = speeds.map((d, index) => {
+      // Filter windspeeds to only include data from the time range
+      const data = d.timeSeries
+        .filter(reading => reading.time > daysAgo)
+        .map(
+          // Return Highcharts Spline dataformat [date, reading]
+          r => [r.time.valueOf(), conversion(r.reading, "m/s", "knot")]
         )
+
+      const nameParts = d.name.split("_")
+      const name = nameParts[nameParts.length - 1]
+      const nameCaps = name[0].toUpperCase() + name.slice(1)
+
+      return <SplineSeries key={index} name={nameCaps} marker={{ enabled: false }} data={data} />
+    })
+
+    /** Array of wind data. Highcharts uses [time, speed, direction] for each reading */
+    const windData: number[][] = []
+
+    /** Make sure our speed and direction data actually exists */
+    if (speeds.length > 0 && directions.length > 0) {
+      const speed = speeds[0]
+      const direction = directions[0]
+
+      let speedTs = speed.timeSeries.filter(r => r.time > daysAgo)
+      let directionTs = direction.timeSeries.filter(r => r.time > daysAgo)
+
+      // cross filter our time series so that we only have matching times
+      const directionTimes = directionTs.map(r => r.time.toISOString())
+      speedTs = speedTs.filter(r => directionTimes.filter(d => d === r.time.toISOString()).length > 0)
+      const speedTimes = speedTs.map(r => r.time.toISOString())
+      directionTs = directionTs.filter(r => speedTimes.filter(d => d === r.time.toISOString()).length > 0)
+
+      if (speedTs.length === directionTs.length) {
+        // Figure out how many samples should be skipped between readings to manage chart density
+        const stride = Math.round(speedTs.length / (this.props.barbsPerDay * this.props.days))
+
+        for (let index = 0; index <= speedTs.length; index += stride) {
+          if (speedTs[index] !== undefined && directionTs[index] !== undefined) {
+            windData.push([
+              directionTs[index].time.valueOf(),
+              round(speedTs[index].reading, 1),
+              directionTs[index].reading
+            ])
+          }
+        }
+      }
     }
+
+    return (
+      <HighchartsChart time={plotOptions.time}>
+        <Chart height={this.props.height} />
+
+        <XAxis type="datetime" />
+
+        <YAxis softMin={0}>
+          <YAxis.Title>Knots</YAxis.Title>
+          {speedsSeries}
+
+          {windData.length > 0 ? <WindBarbSeries name="Direction" color="red" data={windData} /> : null}
+        </YAxis>
+
+        {this.props.legend ? <Legend /> : null}
+
+        <Tooltip formatter={pointFormatter} shared={true} />
+      </HighchartsChart>
+    )
+  }
 }
 
 /** Highcharts enabled WindTimeSeriesChart. See [[WindTimeSeriesChartBase]] for details */
