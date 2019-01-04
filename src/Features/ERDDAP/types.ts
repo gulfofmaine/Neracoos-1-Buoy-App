5ddcdd81c138ee2e182fee9a1b0a91fe57b2947a
Alex Kerney <abk@mac.com>
2019-01-04 16:39:52 -0500

    Adds platform specific alerts. Works on #71

--- src/Features/ERDDAP/types.ts
+++ src/Features/ERDDAP/types.ts
@@ -10,17 +10,12 @@ export interface ERDDAPStoreState {
   errorMessage?: string
   loading: boolean
   platforms: PlatformFeatureWithDatasets[]
-  // platforms: PlatformFeature[]
 }
 
 export const initialStoreState: ERDDAPStoreState = {
   loading: false,
   platforms: []
 }
-
-// export type PlatformFeatureCollection = FeatureCollection & {
-//     features: PlatformFeature[]
-// }
 
 export interface PlatformFeatureCollection {
   features: PlatformFeature[]
@@ -34,16 +29,25 @@ export type PlatformFeature = Feature & {
 
 export type PlatformFeatureWithDatasets = Feature & {
   properties: PlatformProperties & {
+    // forecasts:
     readings: PlatformDataset[]
   }
 }
 
 export interface PlatformProperties {
+  alerts: PlatformAlert[]
   attribution: PlatformAttribution[]
   mooring_site_desc: string
   nbdc_site_id?: string
   // uscg_light_letter?: string
   // watch_circle_radius?: number
+}
+
+export interface PlatformAlert {
+  start_time: string
+  end_time?: string
+  message: string
+  level: string
 }
 
 export interface PlatformTimeSeries {
@@ -63,6 +67,17 @@ export interface PlatformTimeSeries {
 export type PlatformDataset = PlatformTimeSeries & {
   loading: boolean
   loadStartTimes: Date[]
+  error: string
+  readings: ReadingTimeSeries[]
+}
+
+export enum ForecastType {
+  wind,
+  wave
+}
+
+export interface ForecastDataset {
+  loading: boolean
   error: string
   readings: ReadingTimeSeries[]
 }
