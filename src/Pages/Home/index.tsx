14da1a67eed0faac40a24d15babea571a9896f40
Alex Kerney <abk@mac.com>
2019-01-17 14:42:55 -0500

    Fix formatting on home page for mobile

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,26 +1,25 @@
-import * as React from 'react'
-import { Col, Row } from 'reactstrap'
+import * as React from "react"
+import { Col, Row } from "reactstrap"
 
-import { DrupalBlock } from '@app/Features/Drupal'
-import { ErddapMap, ErddapPlatformsLoader } from '@app/Features/ERDDAP'
-
+import { DrupalBlock } from "@app/Features/Drupal"
+import { ErddapMap, ErddapPlatformsLoader } from "@app/Features/ERDDAP"
 
 /**
  * Home page component that combines a map with content from Drupal.
  */
 export const HomePage: React.StatelessComponent<{}> = () => {
-    return (
-        <Row>
-            <Col>
-                <ErddapPlatformsLoader>
-                    <ErddapMap platformId='' />
-                </ErddapPlatformsLoader>
-            </Col>
-            <Col>
-                <DrupalBlock node="node/27" />
-            </Col>
-        </Row>
-    )
+  return (
+    <Row>
+      <Col sm={6}>
+        <ErddapPlatformsLoader>
+          <ErddapMap platformId="" />
+        </ErddapPlatformsLoader>
+      </Col>
+      <Col sm={6}>
+        <DrupalBlock node="node/27" />
+      </Col>
+    </Row>
+  )
 }
 
 export default HomePage
