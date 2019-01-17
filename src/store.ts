604e8bd5c51f5928bcaae037b751b08864f9cd80
Alex Kerney <abk@mac.com>
2019-01-17 13:49:40 -0500

    Change History type

--- src/store.ts
+++ src/store.ts
@@ -1,31 +1,26 @@
-import { connectRouter, routerMiddleware } from 'connected-react-router'
-// import { createBrowserHistory } from 'history'
-import { createHashHistory } from 'history'
-import { applyMiddleware, createStore, Store } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import reduxThunk from 'redux-thunk'
+import { connectRouter, routerMiddleware } from "connected-react-router"
+import { createBrowserHistory } from "history"
+// import { createHashHistory } from 'history'
+import { applyMiddleware, createStore, Store } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import reduxThunk from "redux-thunk"
 
-import rootReducer from './reducer'
+import rootReducer from "./reducer"
 
-import { StoreState } from './constants'
+import { StoreState } from "./constants"
 
 /**
  * Browser history
  */
-// export const history = createBrowserHistory()
-export const history = createHashHistory()
+export const history = createBrowserHistory()
+// export const history = createHashHistory()
 
 /**
  * Global Redux store
  */
 export const store: Store<StoreState> = createStore(
-    connectRouter(history)(rootReducer),
-    composeWithDevTools(
-        applyMiddleware(
-            routerMiddleware(history),
-            reduxThunk
-        )
-    )
+  connectRouter(history)(rootReducer),
+  composeWithDevTools(applyMiddleware(routerMiddleware(history), reduxThunk))
 )
 
 export default store
