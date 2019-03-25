8354069ac52487dc6101ac62b5a5aa4bba0c08c4
Alex Kerney <abk@mac.com>
2019-03-25 15:30:23 -0400

    Add greyed out platforms on the map when data is old

--- src/Features/ERDDAP/Map/index.tsx
+++ src/Features/ERDDAP/Map/index.tsx
@@ -1,54 +1,45 @@
-import { push } from 'connected-react-router'
-import Feature from 'ol/Feature'
-import GeoJSON from 'ol/format/GeoJSON'
-import Layer from 'ol/layer/Layer'
-import VectorLayer from 'ol/layer/Vector'
-import { AttributionLike } from 'ol/source'
-import VectorSource from 'ol/source/Vector'
-import { 
-    Circle,
-    Fill,
-    Stroke,
-    Style
-} from 'ol/style'
-import * as React from 'react'
-import { connect } from 'react-redux'
-import {
-    bindActionCreators,
-    Dispatch
-} from 'redux'
+import { push } from "connected-react-router"
+import Feature from "ol/Feature"
+import GeoJSON from "ol/format/GeoJSON"
+import Layer from "ol/layer/Layer"
+import VectorLayer from "ol/layer/Vector"
+import { AttributionLike } from "ol/source"
+import VectorSource from "ol/source/Vector"
+import { Circle, Fill, Stroke, Style } from "ol/style"
+import * as React from "react"
+import { connect } from "react-redux"
+import { bindActionCreators, Dispatch } from "redux"
 
-import { BaseMap, esriLayers } from '@app/components/Map'
-import { 
-    paths, 
-    StoreState 
-} from '@app/constants'
-import { BoundingBox } from '@app/Shared/regions'
-import { urlPartReplacer } from '@app/Shared/urlParams'
+import { BaseMap, esriLayers } from "@app/components/Map"
+import { paths, StoreState } from "@app/constants"
+import { BoundingBox } from "@app/Shared/regions"
+import { urlPartReplacer } from "@app/Shared/urlParams"
 
-import { PlatformFeatureWithDatasets } from '../types'
-
+import { PlatformFeatureWithDatasets } from "../types"
 
 export interface Props {
-    boundingBox?: BoundingBox
-    platformId: string
+  boundingBox?: BoundingBox
+  platformId: string
 }
 
 export interface ReduxProps {
-    platforms: PlatformFeatureWithDatasets[]
-    push: (url: string) => void
+  platforms: PlatformFeatureWithDatasets[]
+  push: (url: string) => void
 }
 
 export function mapStateToProps({ erddap }: StoreState) {
-    return {
-        platforms: erddap.platforms
-    }
+  return {
+    platforms: erddap.platforms
+  }
 }
 
-export const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
-    push
-}, dispatch)
-
+export const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      push
+    },
+    dispatch
+  )
 
 /** If the window is narrower than 800px we should increase object sizes */
 const adjustPxWidth = 800
@@ -56,28 +47,28 @@ const adjustPxWidth = 800
 /**
  * Create the styles that our platforms will use
  * @param selected Is this the style for a selected platform
+ * @param old Is the data old and should the platform be greyed out
  */
-function makeStyle(selected: boolean): Style {
-    let radius: number
-    if (selected) {
-        radius = window.innerWidth > adjustPxWidth ? 10 : 15
-    } else {
-        radius = window.innerWidth > adjustPxWidth ? 5 : 10
-    }
-    const opacity = selected ? '0.8' : '0.4'
-    return new Style({
-        image: new Circle({
-            fill: new Fill({
-                // color: 'rgba(255, 153, 0, ' + selected ? '0.8)' : '0.4)'
-                color: `rgba(255, 153, 0, ${opacity})`
-            }),
-            radius,
-            stroke: new Stroke({
-                color: 'red',
-                width: 1
-            })
-        })
+function makeStyle(selected: boolean, old: boolean = false): Style {
+  let radius: number
+  if (selected) {
+    radius = window.innerWidth > adjustPxWidth ? 10 : 15
+  } else {
+    radius = window.innerWidth > adjustPxWidth ? 5 : 10
+  }
+  const opacity = selected ? "0.8" : "0.4"
+  return new Style({
+    image: new Circle({
+      fill: new Fill({
+        color: old ? "grey" : `rgba(255, 153, 0, ${opacity})`
+      }),
+      radius,
+      stroke: new Stroke({
+        color: old ? "grey" : "red",
+        width: 1
+      })
     })
+  })
 }
 
 /**
@@ -86,23 +77,26 @@ function makeStyle(selected: boolean): Style {
  * @param style Should the layer be fore selected platforms
  */
 function makePlatformLayer(platforms: PlatformFeatureWithDatasets[], style: Style): VectorLayer {
-    const attribution: AttributionLike = 'NERACOOS'
+  const attribution: AttributionLike = "NERACOOS"
 
-    const platformSource = new VectorSource({
-        attributions: [attribution],
-        features: (new GeoJSON()).readFeatures({
-            features: platforms,
-            type: 'FeatureCollection'
-        }, {
-            dataProjection: 'EPSG:4326',
-            featureProjection: 'EPSG:3857'
-        })
-    })
+  const platformSource = new VectorSource({
+    attributions: [attribution],
+    features: new GeoJSON().readFeatures(
+      {
+        features: platforms,
+        type: "FeatureCollection"
+      },
+      {
+        dataProjection: "EPSG:4326",
+        featureProjection: "EPSG:3857"
+      }
+    )
+  })
 
-    return new VectorLayer({
-        source: platformSource,
-        style
-    })
+  return new VectorLayer({
+    source: platformSource,
+    style
+  })
 }
 
 /**
@@ -112,64 +106,75 @@ function makePlatformLayer(platforms: PlatformFeatureWithDatasets[], style: Styl
  * @param platformId Selected platform ID name
  */
 export function makeLayers(layers: Layer[], platforms: PlatformFeatureWithDatasets[], platformId: string): Layer[] {
-    /** platform styles */
-    layers = [...layers]
-    const platformStyle = makeStyle(false)
-    const selectedStyle = makeStyle(true)
+  /** platform styles */
+  layers = [...layers]
+  const platformStyle = makeStyle(false)
+  const selectedStyle = makeStyle(true)
+  const oldStyle = makeStyle(false, true)
 
-    const filteredPlatforms = platforms.filter((p) => p.id !== platformId)
-    const selectedPlatforms = platforms.filter((p) => p.id === platformId)
+  // tslint:disable-next-line:no-debugger
+  //   debugger
 
-    if (filteredPlatforms.length > 0) {
-        layers.push(makePlatformLayer(filteredPlatforms, platformStyle))
-    }
-    if (selectedPlatforms.length > 0) {
-        layers.push(makePlatformLayer(selectedPlatforms, selectedStyle))
-    }
+  const oldPlatforms = platforms.filter(p => p.id !== platformId && p.properties.readings.every(r => r.time === null))
+  const filteredPlatforms = platforms.filter(
+    p => p.id !== platformId && p.properties.readings.some(r => r.time !== null)
+  )
+  const selectedPlatforms = platforms.filter(p => p.id === platformId)
 
-    return layers
+  if (oldPlatforms.length > 0) {
+    layers.push(makePlatformLayer(oldPlatforms, oldStyle))
+  }
+
+  if (filteredPlatforms.length > 0) {
+    layers.push(makePlatformLayer(filteredPlatforms, platformStyle))
+  }
+  if (selectedPlatforms.length > 0) {
+    layers.push(makePlatformLayer(selectedPlatforms, selectedStyle))
+  }
+
+  return layers
 }
 
 /** Basemap layers */
-const baseLayers: Layer[] = [
-    esriLayers.EsriOceanBasemapLayer,
-    esriLayers.EsriOceanReferenceLayer
-]
-
+const baseLayers: Layer[] = [esriLayers.EsriOceanBasemapLayer, esriLayers.EsriOceanReferenceLayer]
 
 /**
  * ErddapMapBase provides a map with platforms focused on the Gulf of Maine by default
  */
 export class ErddapMapBase extends React.Component<Props & ReduxProps, object> {
-    constructor(props: Props & ReduxProps) {
-        super(props)
+  constructor(props: Props & ReduxProps) {
+    super(props)
 
-        this.onClick = this.onClick.bind(this)
-    }
+    this.onClick = this.onClick.bind(this)
+  }
 
-    public render() {
-        return (
-            <BaseMap
-                lon={-68.5}
-                lat={43.5}
-                startZoom={6}
-                layers={makeLayers(baseLayers, this.props.platforms, this.props.platformId)}
-                boundingBox={this.props.boundingBox}
-                onClick={this.onClick} />
-        )
-    }
+  public render() {
+    return (
+      <BaseMap
+        lon={-68.5}
+        lat={43.5}
+        startZoom={6}
+        layers={makeLayers(baseLayers, this.props.platforms, this.props.platformId)}
+        boundingBox={this.props.boundingBox}
+        onClick={this.onClick}
+      />
+    )
+  }
 
-    /**
-     * Handle clicks from OpenLayers
-     * @param feature OpenLayers Feature
-     */
-    protected onClick(feature: Feature) {
-        const name: string = feature.getId()
-        const url = urlPartReplacer(paths.platforms.platform, ':id', name)
+  /**
+   * Handle clicks from OpenLayers
+   * @param feature OpenLayers Feature
+   */
+  protected onClick(feature: Feature) {
+    const name: string = feature.getId()
+    const url = urlPartReplacer(paths.platforms.platform, ":id", name)
 
-        this.props.push(url)
-    }
+    this.props.push(url)
+  }
 }
 
 /** Redux connected ErddapMap. See [[ErddapMapBase]] for details. */
-export const ErddapMap = connect(mapStateToProps, mapDispatchToProps)(ErddapMapBase)
+export const ErddapMap = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ErddapMapBase)
