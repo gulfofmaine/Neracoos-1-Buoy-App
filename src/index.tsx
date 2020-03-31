0c62e6b26501d9eda609b068b947e1e773cd85c6
Alex Kerney <abk@mac.com>
2020-03-31 13:54:23 -0400

    Update Cypress and disable Sentry during tests
    #5mg7zq

--- src/index.tsx
+++ src/index.tsx
@@ -32,20 +32,23 @@ window.moment = moment
 // tslint:disable-next-line:no-var-requires
 const packageJson = require("../package.json")
 
-Sentry.init({
-  dsn: "https://eab04522f42c4efab9d5bfe7d8594e9c@sentry.io/1270344",
-  beforeSend(event: Event) {
-    if (event.exception) {
-      if (event.extra && "skipDialog" in event.extra) {
-        // pass
-      } else {
-        Sentry.showReportDialog()
+// Disable Sentry if we are testing with Cypress
+if (!(window as any).Cypress) {
+  Sentry.init({
+    dsn: "https://eab04522f42c4efab9d5bfe7d8594e9c@sentry.io/1270344",
+    beforeSend(event: Event) {
+      if (event.exception) {
+        if (event.extra && "skipDialog" in event.extra) {
+          // pass
+        } else {
+          Sentry.showReportDialog()
+        }
       }
-    }
-    return event
-  },
-  release: packageJson.version
-})
+      return event
+    },
+    release: packageJson.version
+  })
+}
 
 /**
  * Render our root element of the App.
