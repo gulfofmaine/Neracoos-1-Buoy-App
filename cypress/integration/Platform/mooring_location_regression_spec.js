7ee6a8bf9ed892082e83af4f0be81b5d2f8adee7
Alex Kerney <abk@mac.com>
2020-05-01 16:03:11 -0400

    Fix tests

--- cypress/integration/Platform/mooring_location_regression_spec.js
+++ cypress/integration/Platform/mooring_location_regression_spec.js
@@ -2,12 +2,12 @@
 
 describe("Mooring location regressions", () => {
   it("Finds the correct location for B01", () => {
-    cy.visit("/platform/B01")
+    cy.visit("/platform/B01%20-%2044030")
     cy.get(".card-text").contains("Western Maine Shelf")
   })
 
   it("Finds the correct location for I01", () => {
-    cy.visit("/platform/I01")
+    cy.visit("/platform/I01%20-%2044034")
     cy.get(".card-text").contains("Eastern Maine Shelf")
   })
 })
