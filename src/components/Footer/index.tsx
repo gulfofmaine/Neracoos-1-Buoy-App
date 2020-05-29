ce2ffe10c058d52c01cd221ae8cb399d1be99a22
Alex Kerney <abk@mac.com>
2020-05-29 14:42:43 -0400

    Update footer styling
    #64fg0k[in review]

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react"
 import { Col, Row } from "reactstrap"
 
-import { SignalFlag } from "components/SignalFlag"
+import neracoos from "./NERACOOS_horizontal_logo_acronym title_yellowline.png"
+import gmri from "./gmri.jpg"
 
 const imageStyle = { height: "40px", margin: ".2rem" }
 const logoStyle = { ...imageStyle, height: "80px", marginRight: ".5rem" }
@@ -19,24 +20,24 @@ export const Footer: React.SFC = () => (
     </Row>
     <Row style={textStyle}>
       <Col md={colSize} className="mx-auto text-center">
-        Product of <a href="http://www.neracoos.org/">NERACOOS.org</a> developed and maintained by the{" "}
-        <a href="https://www.gmri.org/our-work/research/our-approach/ocean-data">Gulf of Maine Research Institute</a>
+        Product of <a href="http://www.neracoos.org/">NERACOOS.org</a> - Developed by the{" "}
+        <a href="https://www.gmri.org/our-work/research/our-approach/ocean-data">
+          Gulf of Maine Research Institute Ocean Data Products Team
+        </a>
       </Col>
     </Row>
     <Row style={textStyle}>
       <Col md={colSize} className="mx-auto text-center">
-        <a href="https://www.gmri.org/">
+        <a href="http://neracoos.org">
+          <img src={neracoos} style={logoStyle} alt="NERACOOS" title="NERACOOS" />
+        </a>
+        <a href="https://www.gmri.org/our-work/research/our-approach/ocean-data">
           <img
-            src="http://gmri.org/sites/default/files/gmri_logo_tag.jpg"
+            src={gmri}
             style={logoStyle}
             alt="Gulf of Maine Research Institute"
             title="Gulf of Maine Research Institute"
           />
-        </a>
-        <a href="https://www.gmri.org/our-work/research/our-approach/ocean-data" style={{ display: "inline-block" }}>
-          <SignalFlag character="o" title="O for Ocean" style={imageStyle} />
-          <SignalFlag character="d" title="D for Data" style={imageStyle} />
-          <SignalFlag character="p" title="P for Products" style={imageStyle} />
         </a>
       </Col>
     </Row>
