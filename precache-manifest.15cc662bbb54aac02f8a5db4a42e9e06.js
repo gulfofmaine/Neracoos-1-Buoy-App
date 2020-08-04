c8a92f0cdfba8800f72ec40f1cca896addb4feda
abkfenris <abk@mac.com>
2020-08-04 14:23:10 +0000

    Deploying to gh-pages from  @ bcc1583e12ac006a48b21589c0ccb38df3ab4c8f 🚀

---
+++ precache-manifest.15cc662bbb54aac02f8a5db4a42e9e06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a81aae10202cdab42c9c28be5fa756d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c004d176eebdb8322334.bundle.js"
+  },
+  {
+    "url": "runtime~main.c004d176eebdb8322334.bundle.js"
+  },
+  {
+    "revision": "29edca422991fd3262cc",
+    "url": "static/css/main.f75da718.chunk.css"
+  },
+  {
+    "revision": "84ea72439cb2f54136a9",
+    "url": "static/css/vendors~main.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "e4468413f999e4f2d452b3cbc3d11617",
+    "url": "static/media/NERACOOS_horizontal_logo_acronym title_yellowline.e4468413.png"
+  },
+  {
+    "revision": "e6c17586d23332c0b8cae94ecec17706",
+    "url": "static/media/gmri.e6c17586.jpg"
+  },
+  {
+    "url": "vendors~main.c004d176eebdb8322334.bundle.js"
+  },
+  {
+    "url": "vendors~main.c004d176eebdb8322334.bundle.js.LICENSE.txt"
+  }
+]);
