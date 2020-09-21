12abe7191bf79dec975808cda64460834612e08f
Alex Kerney <abk@mac.com>
2020-09-21 15:22:13 -0400

    Increase time window for current conditions, and fix deployment issues

--- src/Features/ERDDAP/Platform/Observations/CurrentConditions/index.tsx
+++ src/Features/ERDDAP/Platform/Observations/CurrentConditions/index.tsx
@@ -7,6 +7,7 @@ import { Row } from "reactstrap"
 
 import { PlatformFeature } from "../../../types"
 
+import { conditions } from "./conditions"
 import { DataCard } from "./data_card"
 import { WindCard } from "./wind"
 
@@ -23,32 +24,12 @@ export const ErddapCurrentPlatformConditions: React.FunctionComponent<Props> = (
   <Row>
     <WindCard platform={platform} />
 
-    <DataCard platform={platform} data_types={["air_temperature", "air_temperature_1m"]} />
-    <DataCard
-      platform={platform}
-      data_types={["air_pressure", "barometric_pressure", "sea_level_pressure", "air_pressure_at_sea_level"]}
-    />
-    <DataCard
-      platform={platform}
-      data_types={[
-        "sea_surface_wave_significant_height",
-        "significant_height_of_wind_and_swell_waves",
-        "significant_wave_height",
-        "significant_height_of_wind_and_swell_waves_3",
-      ]}
-    />
-    <DataCard
-      platform={platform}
-      data_types={[
-        "period",
-        "sea_surface_swell_wave_period",
-        "dominant_wave_period",
-        "average_wave_period",
-        "dominant_wave_period_3",
-      ]}
-    />
-    <DataCard platform={platform} data_types={["mean_wave_direction", "sea_surface_wave_to_direction"]} />
-    <DataCard platform={platform} data_types={["sea_surface_temperature", "sea_water_temperature"]} />
-    <DataCard platform={platform} data_types={["visibility_in_air"]} />
+    <DataCard platform={platform} data_types={conditions.airTemp} />
+    <DataCard platform={platform} data_types={conditions.airPressure} />
+    <DataCard platform={platform} data_types={conditions.waveHeight} />
+    <DataCard platform={platform} data_types={conditions.wavePeriod} />
+    <DataCard platform={platform} data_types={conditions.waveDirection} />
+    <DataCard platform={platform} data_types={conditions.waterTemp} />
+    <DataCard platform={platform} data_types={conditions.visibility} />
   </Row>
 )
