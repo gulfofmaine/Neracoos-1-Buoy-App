3cc8194ae3074da801b9939691b74beb4ddad160
Alex Kerney <abk@mac.com>
2020-09-22 16:33:15 -0400

    Add Mayflower data types
    CU-64c998

---
+++ src/Features/Units/Converter/data_types/relative_humidity.spec.ts
@@ -0,0 +1,25 @@
+import { UnitSystem } from "../../types"
+import { data_types } from "./index"
+
+const { relative_humidity } = data_types
+
+const source_value = 74.5
+
+const displayName = "Percent"
+
+describe("period conversions", () => {
+  it("to english", () => {
+    const result = relative_humidity.convertTo(source_value, UnitSystem.english)
+    expect(result).toBeCloseTo(source_value)
+  })
+
+  it("to metric", () => {
+    const result = relative_humidity.convertTo(source_value, UnitSystem.metric)
+    expect(result).toBeCloseTo(source_value)
+  })
+
+  it("display names", () => {
+    expect(relative_humidity.displayName(UnitSystem.english)).toBe(displayName)
+    expect(relative_humidity.displayName(UnitSystem.metric)).toBe(displayName)
+  })
+})
---
+++ src/Features/Units/Converter/data_types/sea_water_velocity_m.spec.ts
@@ -0,0 +1,28 @@
+import { UnitSystem } from "../../types"
+import { data_types } from "./index"
+
+const source_value = 10
+
+const { sea_water_velocity_m } = data_types
+
+describe("sea_water_velocity_m conversions", () => {
+  it("to english", () => {
+    const result = sea_water_velocity_m.convertTo(source_value, UnitSystem.english)
+    expect(result).toBeCloseTo(19.4384)
+  })
+
+  it("to metric", () => {
+    const result = sea_water_velocity_m.convertTo(source_value, UnitSystem.metric)
+    expect(result).toBeCloseTo(source_value)
+  })
+
+  it("to mariners", () => {
+    const result = sea_water_velocity_m.convertTo(source_value, UnitSystem.mariners)
+    expect(result).toBeCloseTo(19.4384)
+  })
+
+  it("display names", () => {
+    expect(sea_water_velocity_m.displayName(UnitSystem.english)).toBe("Knots")
+    expect(sea_water_velocity_m.displayName(UnitSystem.metric)).toBe("Meters/Second")
+  })
+})
