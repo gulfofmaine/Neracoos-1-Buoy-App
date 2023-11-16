4831bf469e18f1afe12882d961ad8ff164c9508c
Adelle Pitsas <>
2023-11-16 11:49:37 -0500

    fix prettier

--- src/Features/ERDDAP/Superlatives/index.tsx
+++ src/Features/ERDDAP/Superlatives/index.tsx
@@ -70,7 +70,7 @@ export const ShowSuperlatives: React.FunctionComponent<ShowSuperlativesProps> = 
         return
       }
     }
-  }, [])
+  }, [platforms])
 
   useEffect(() => {
     for (let hours = 0; hours < backoffHours; hours++) {
@@ -85,7 +85,7 @@ export const ShowSuperlatives: React.FunctionComponent<ShowSuperlativesProps> = 
         return
       }
     }
-  }, [])
+  }, [platforms])
 
   return (
     <Card style={{ marginTop: "1rem", marginBottom: "1rem" }}>
