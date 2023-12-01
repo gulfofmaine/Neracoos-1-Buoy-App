73389261f4ac06d49f9bc5d8b91ccf69cff3e55d
Adelle Pitsas <>
2023-12-01 11:14:27 -0500

    only render on client to avoid hydration issues

--- app/(platformMap)/@sidebar/platform/[platformId]/page.tsx
+++ app/(platformMap)/@sidebar/platform/[platformId]/page.tsx
@@ -1,13 +1,21 @@
 import { DehydratedPlatforms } from "Features/ERDDAP/hooks/DehydrateComponent"
-
 import { PlatformInfo } from "Pages/Platforms/platformInfo"
+import React, { useEffect, useState } from "react"
 
 export default async function PlatformSidebar({ params }: { params: { platformId: string } }) {
   const platformId = decodeURIComponent(params.platformId)
+  const [isClient, setIsClient] = useState(false)
 
-  return (
-    <DehydratedPlatforms>
-      <PlatformInfo id={platformId} />
-    </DehydratedPlatforms>
-  )
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (isClient) {
+    return (
+      <DehydratedPlatforms>
+        <PlatformInfo id={platformId} />
+      </DehydratedPlatforms>
+    )
+  }
+  return null
 }
