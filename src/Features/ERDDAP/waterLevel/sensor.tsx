c3dfa9bffa3c7757e7d6203af0ab09d54c5f9bb4
Adelle Pitsas <apitsas@gmri.org>
2024-01-12 15:40:25 -0500

    change filter for platforms that have water level measurements

--- src/Features/ERDDAP/waterLevel/sensor.tsx
+++ src/Features/ERDDAP/waterLevel/sensor.tsx
@@ -1,10 +1,11 @@
 import { PlatformFeatureCollection } from "../types"
 
 export const filterForSensors = (platforms: PlatformFeatureCollection) => {
-  return platforms?.features.filter(
-    (p) =>
-      p.properties.attribution[0].attribution === "NERACOOS" ||
-      p.properties.attribution[0].attribution === "Gulf of Maine Research Institute" ||
-      p.properties.attribution[0].attribution === "Hohonu",
-  )
+  console.log("chicken", platforms.features[0].properties.readings)
+  return platforms?.features.filter((p) => {
+    return (
+      p.properties.readings.find((r) => r.data_type.short_name === "SWL") ||
+      p.properties.attribution[0].attribution === "Hohonu"
+    )
+  })
 }
