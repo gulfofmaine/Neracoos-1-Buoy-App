818b4ef7f09056311b1ef007f84354632ee495bf
Alex Kerney <abk@mac.com>
2024-01-18 12:34:21 -0500

    Setup Spotlight to capture dev exceptions
    This sets up https://spotlightjs.com/ catch exceptions that would otherwise go up to Sentry in development.
    
    Additionally the Sentry exception handling in development is disabled and now depends on DSNs that are baked into/supplied for the images rather than a DSN that is hard coded. This also removed a DSN that is public and new Sentry DSNs have been generated.

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -13,7 +13,8 @@ import * as Sentry from "@sentry/nextjs"
 const packageJson = require("./package.json")
 
 Sentry.init({
-  dsn: "https://eab04522f42c4efab9d5bfe7d8594e9c@o181061.ingest.sentry.io/1270344",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN ?? "__dsn__",
+  spotlight: process.env.NODE_ENV === "development",
 
   release: `v${packageJson.version}`,
 
