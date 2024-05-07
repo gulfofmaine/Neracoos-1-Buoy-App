0762e6e5d556d8a8193116ba2176436431be03ca
Adelle Pitsas <apitsas@gmri.org>
2024-05-07 15:21:32 -0400

    fix timeframe selector disappearing and add validation to timeframe

--- src/Features/ERDDAP/waterLevel/timeframeSelector.tsx
+++ src/Features/ERDDAP/waterLevel/timeframeSelector.tsx
@@ -10,6 +10,7 @@ export const TimeframeSelector = ({ graphFuture }: { graphFuture: boolean }) => 
   const params = useParams()
   const [startTime, setStartTime] = useState<any>(decodeURIComponent(params.startTime as string))
   const [endTime, setEndTime] = useState<any>(decodeURIComponent(params.endTime as string))
+  const [validDate, setValidDate] = useState<boolean>(true)
   const startTimeParams = decodeURIComponent(params.startTime as string)
   const endTimeParams = decodeURIComponent(params.endTime as string)
 
@@ -17,6 +18,26 @@ export const TimeframeSelector = ({ graphFuture }: { graphFuture: boolean }) => 
     const endTime = new Date(date)
     setEndTime(getIsoForPicker(endTime))
   }
+
+  const validateTimeframe = (start, end) => {
+    //check if timeFrame spans more than two weeks
+    if ((new Date(end).getTime() - new Date(start).getTime()) / 86400000 > 14) {
+      return false
+    }
+    //check if timeframe is recent enough
+    if (new Date(start).getFullYear() < 2023 || new Date(end).getFullYear() < 2023) {
+      return false
+    }
+    //check if endDate is before startDate
+    if (new Date(end).getTime() - new Date(start).getTime() < 0) {
+      return false
+    }
+    return true
+  }
+
+  useEffect(() => {
+    setValidDate(validateTimeframe(startTime, endTime))
+  }, [startTime, endTime])
 
   useEffect(() => {
     setStartTime(getIsoForPicker(new Date(startTimeParams)))
@@ -28,52 +49,51 @@ export const TimeframeSelector = ({ graphFuture }: { graphFuture: boolean }) => 
       <Col style={{ width: "100%", margin: 0 }}>
         <h6 style={{ width: "100%", paddingTop: "10px", fontWeight: "bold" }}>Timeframe: </h6>
       </Col>
-      {startTime && endTime && (
-        <div>
-          <Col
-            style={{
-              margin: 0,
-              padding: 0,
-              width: "100%",
-              display: "flex",
-              alignItems: "end",
-              justifyContent: "space-between",
-            }}
-          >
-            <label style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
-              Start date:
-              <input
-                type="date"
-                id="start"
-                name="timeframe-start"
-                max={getToday()}
-                value={startTime}
-                onChange={(e) => setStartTime(e.target.value)}
-              />
-            </label>
-            <label style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
-              End date:
-              <input
-                type="date"
-                id="end"
-                name="timeframe-end"
-                min={startTime}
-                max={graphFuture ? undefined : getToday()}
-                value={endTime}
-                onChange={(e) => getEndTime(e.target.value)}
-              />
-            </label>
+      <div>
+        <Col
+          style={{
+            margin: 0,
+            padding: 0,
+            width: "100%",
+            display: "flex",
+            alignItems: "end",
+            justifyContent: "space-between",
+          }}
+        >
+          <label style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+            Start date:
+            <input
+              type="date"
+              id="start"
+              name="timeframe-start"
+              max={getToday()}
+              value={startTime}
+              onChange={(e) => setStartTime(e.target.value)}
+            />
+          </label>
+          <label style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+            End date:
+            <input
+              type="date"
+              id="end"
+              name="timeframe-end"
+              min={startTime}
+              max={graphFuture ? undefined : getToday()}
+              value={endTime}
+              onChange={(e) => getEndTime(e.target.value)}
+            />
+          </label>
+
+          <Button color="primary" size="sm" disabled={!startTime || !endTime || !validDate}>
             <a
               href={`/water-level/sensor/${params.sensorId}/${startTime}/${endTime}/${params.datum}`}
-              style={{ marginLeft: "10px" }}
+              style={{ color: "white", textDecoration: "none", width: "100%", height: "100%" }}
             >
-              <Button color="primary" size="sm" style={{ whiteSpace: "nowrap" }}>
-                Plot Dates
-              </Button>
+              Plot Dates
             </a>
-          </Col>
-        </div>
-      )}
+          </Button>
+        </Col>
+      </div>
     </Row>
   )
 }
