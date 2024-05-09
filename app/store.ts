2765a0e96fe8fdee572097816789635bb0462390
Adelle Pitsas <apitsas@gmri.org>
2024-05-09 15:46:34 -0400

    persist unit selection on page refresh and new platform/sensor selection

--- app/store.ts
+++ app/store.ts
@@ -2,10 +2,39 @@ import { configureStore } from "@reduxjs/toolkit"
 
 import { unitReducer as unit } from "Features/Units"
 
+const preloadedState = loadFromLocalStorage()
+
 export const store = configureStore({
   reducer: {
     unit,
   },
+  preloadedState: {
+    unit: preloadedState,
+  },
+})
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState().unit)
 })
 
 export type RootState = ReturnType<typeof store.getState>
+
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state)
+    sessionStorage.setItem("unit", serializedState)
+  } catch (e) {
+    console.error("Could not save state", e)
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = sessionStorage.getItem("unit")
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.error("Could not load state", e)
+    return undefined
+  }
+}
