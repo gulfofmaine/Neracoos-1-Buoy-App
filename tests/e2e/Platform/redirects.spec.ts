47a35c2d008028e75d80f3d6518a1f11e276edce
Alex Kerney <abk@mac.com>
2024-07-29 14:14:42 -0400

    Post-pre-commit

--- tests/e2e/Platform/redirects.spec.ts
+++ tests/e2e/Platform/redirects.spec.ts
@@ -1,11 +1,11 @@
-import {expect, test} from "@playwright/test"
+import { expect, test } from "@playwright/test"
 
 test.describe("Platform redirects", () => {
-    // Redirects for tide gauge signs https://github.com/gulfofmaine/Neracoos-1-Buoy-App/issues/3011
-    test("Can redirect to platform page", async ({page}) => {
-        await page.goto("/s/port-clyde-tide-gauge")
-        await expect(await page.getByText("Port Clyde Tide Gauge").first()).toBeVisible()
-        await expect(await page.getByText("Mean Lower Low Water").first()).toBeVisible()
-        await expect(page.url()).toBe("http://127.0.0.1:3000/platform/Port%20Clyde%20Tide%20Gauge")
-    })
+  // Redirects for tide gauge signs https://github.com/gulfofmaine/Neracoos-1-Buoy-App/issues/3011
+  test("Can redirect to platform page", async ({ page }) => {
+    await page.goto("/s/port-clyde-tide-gauge")
+    await expect(await page.getByText("Port Clyde Tide Gauge").first()).toBeVisible()
+    await expect(await page.getByText("Mean Lower Low Water").first()).toBeVisible()
+    await expect(page.url()).toBe("http://127.0.0.1:3000/platform/Port%20Clyde%20Tide%20Gauge")
+  })
 })
