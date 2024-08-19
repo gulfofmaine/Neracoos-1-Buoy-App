bd404df35ae2262fd00435d6bf0dd315e2694e0a
Alex Kerney <abk@mac.com>
2024-08-19 18:04:33 -0400

    Tweak snapshot actions

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,15 +1,15 @@
-import type { TestRunnerConfig } from '@storybook/test-runner';
+import type { TestRunnerConfig } from "@storybook/test-runner"
 
 const config: TestRunnerConfig = {
-    tags: {
-        skip: ['skip-snapshot']
-    },
-    async postVisit(page, context) {
-        // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
-        const elementHandler = await page.$('#storybook-root');
-        const innerHTML = await elementHandler.innerHTML();
-        expect(innerHTML).toMatchSnapshot();
-    },
-};
+  tags: {
+    skip: ["skip-snapshot"],
+  },
+  async postVisit(page, context) {
+    // the #storybook-root element wraps the story. In Storybook 6.x, the selector is #root
+    const elementHandler = await page.$("#storybook-root")
+    const innerHTML = await elementHandler.innerHTML()
+    expect(innerHTML).toMatchSnapshot()
+  },
+}
 
-export default config;
+export default config
