b1ef91e5075a32e55a1e6b0bed01d390452b6707
Alex Kerney <abk@mac.com>
2024-08-19 16:52:57 -0400

    Sentry and some other dev updates
    Updated Sentry to v8 and trying to enable sourcemaps to make errors a little more understandable.
    
    Added a global-error to try to make things nicer when it fully explodes.
    
    Also removed yarn.lock as it was confusing some tools such as Sentry's wizard now that we've switched back to NPM.

--- next.config.js
+++ next.config.js
@@ -43,34 +43,36 @@ const nextConfig = {
 
 const { withSentryConfig } = require("@sentry/nextjs")
 
-module.exports = withSentryConfig(
-  nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+module.exports = withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "gulf-of-maine-research-institu",
-    project: "neracoos-mariners-dashboard",
+  org: "gulf-of-maine-research-institu",
+  project: "neracoos-mariners-dashboard",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
   },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  },
-)
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+})
