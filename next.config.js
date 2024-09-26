aaba6cbed76c4b19c545cd380a499a23579b6f08
Alex Kerney <abk@mac.com>
2024-09-26 17:38:31 -0400

    Tweak Sentry config for v8
    There were a few more changes that I missed for Sentry/Next v8

--- next.config.js
+++ next.config.js
@@ -45,34 +45,29 @@ const nextConfig = {
 
 const { withSentryConfig } = require("@sentry/nextjs")
 
-module.exports = withSentryConfig(
-  nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+module.exports = withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "gulf-of-maine-research-institu",
-    project: "neracoos-mariners-dashboard",
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Suppresses source map uploading logs during build
+  silent: true,
+  org: "gulf-of-maine-research-institu",
+  project: "neracoos-mariners-dashboard",
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: "/monitoring",
+  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+  tunnelRoute: "/monitoring",
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  },
-)
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+})
