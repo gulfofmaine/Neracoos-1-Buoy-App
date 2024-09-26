5f4cf35b928560c464d6cdd2fa00f3b103d76aba
Alex Kerney <abk@mac.com>
2024-09-26 16:36:19 -0400

    Add Global Error, Sentry sourcemaps, and silences a warning
    Adds a global error handler for when things explode even more. 
    - https://nextjs.org/docs/app/api-reference/file-conventions/error#global-errorjs
    - https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#react-render-errors-in-app-router
    
    Additionally should enable uploading of sourcemaps to Sentry so we can see the source of explosions better.
    
    Silences a few Sentry Error on build.
    
    Replaces Sentry parts of  #3069

---
+++ app/global-error.tsx
@@ -0,0 +1,50 @@
+"use client"
+import * as Sentry from "@sentry/nextjs"
+import Image from "next/image"
+import { useEffect, useRef } from "react"
+import { Button, Col, Row } from "reactstrap"
+
+import n_walkabout from "./n_walkabout.png"
+
+export default function GlobalError({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  let eventId = useRef("")
+
+  useEffect(() => {
+    eventId.current = Sentry.captureException(error)
+  }, [error])
+
+  return (
+    <html>
+      <body>
+        <Row>
+          <Col md={{ offset: 2, size: 8 }}>
+            <h1>The data has escaped</h1>
+            <Image src={n_walkabout} style={{ maxWidth: "100%", height: "auto" }} alt="Track of a lost buoy" />
+            <p>Despite our best intentions, something has gone wrong while trying to display the data.</p>
+
+            <p>
+              If you saw a <b>It looks like we&apos;re having issues</b> form, we hope that you filled it out, as it
+              will help us fix the issue. Otherwise you can email{" "}
+              <a href="mailto:info@neracoos.org">info@neracoos.org</a> with information about the error, or try to
+              launch the form.
+            </p>
+
+            <Button color="primary" onClick={() => Sentry.showReportDialog({ eventId: eventId.current })}>
+              I&apos;ve got an idea what went wrong, please show me the form
+            </Button>
+
+            <p style={{ paddingTop: "1rem" }}>
+              Sometimes issues are short lived, so if you give it a minute, then try reloading the page, hopefully you
+              will be able to get to the data. If you can&apos;t and this error shows up again, please fill out the
+              form, or email us for help.
+            </p>
+
+            <Button color="danger" onClick={() => reset()}>
+              Try again
+            </Button>
+          </Col>
+        </Row>
+      </body>
+    </html>
+  )
+}
