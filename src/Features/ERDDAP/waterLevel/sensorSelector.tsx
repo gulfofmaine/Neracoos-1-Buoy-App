42d8d9dfbb2fe7250662d3fd57179cc98b4d1514
Adelle Pitsas <apitsas@gmri.org>
2024-10-10 15:10:34 -0400

    alphabetize station dropdown

--- src/Features/ERDDAP/waterLevel/sensorSelector.tsx
+++ src/Features/ERDDAP/waterLevel/sensorSelector.tsx
@@ -10,7 +10,7 @@ export const WaterLevelSensorSelector = ({ sensors }) => {
   const [sensorOptions, setSensorOptions] = useState()
   const params = useParams()
   const searchParams = useSearchParams()
-  const sensorId = useDecodedUrl(params.sensorId as string)
+  const sensor = sensors.find((s) => s.id === useDecodedUrl(params.sensorId as string))
   const endTime = searchParams.get("end")
   const startTime = searchParams.get("start")
 
@@ -20,21 +20,27 @@ export const WaterLevelSensorSelector = ({ sensors }) => {
 
   useEffect(() => {
     if (sensors) {
-      const options = sensors.map((p, index) => {
-        return (
-          <Link
-            key={`dropdown-${index}`}
-            onClick={() => close()}
-            href={{
-              pathname: `/water-level/sensor/${p.id as string}`,
-              query: buildSearchParamsQuery(startTime as string, endTime as string, "datum_mllw_meters"),
-            }}
-            className="list-group-item list-group-item-action"
-          >
-            {p.id}
-          </Link>
-        )
-      })
+      const options = sensors
+        .sort((a, b) => {
+          if (a.id < b.id) {
+            return -1
+          } else return 1
+        })
+        .map((p, index) => {
+          return (
+            <Link
+              key={`dropdown-${index}`}
+              onClick={() => close()}
+              href={{
+                pathname: `/water-level/sensor/${p.id as string}`,
+                query: buildSearchParamsQuery(startTime as string, endTime as string, "datum_mllw_meters"),
+              }}
+              className="list-group-item list-group-item-action"
+            >
+              {p.id} {p.properties.station_name ? `- ${p.properties.station_name}` : null}
+            </Link>
+          )
+        })
       setSensorOptions(options)
     }
   }, [sensors])
@@ -51,7 +57,7 @@ export const WaterLevelSensorSelector = ({ sensors }) => {
           style={{ border: "1px solid black", borderRadius: "7px" }}
         >
           <DropdownToggle color={"#FFFFFF"} caret={true}>
-            {sensorId}
+            {sensor.id} {sensor.properties.station_name ? `- ${sensor.properties.station_name}` : null}
           </DropdownToggle>
           {sensorOptions && (
             <DropdownMenu end={true} style={{ padding: 0, border: "1px", maxHeight: "215px", overflow: "scroll" }}>
