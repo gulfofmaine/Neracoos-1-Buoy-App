8d43abbddbc21131ada97f06b14cda29f1ab2ae6
Adelle Pitsas <apitsas@gmri.org>
2024-10-24 16:16:15 -0400

    make codacy happy

--- src/Features/ERDDAP/List/waterSensorList.tsx
+++ src/Features/ERDDAP/List/waterSensorList.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { getIsoForPicker, threeDaysAgoRounded, weeksInFuture } from "Shared/time"
 import Link from "next/link"
 import { useEffect, useState } from "react"
