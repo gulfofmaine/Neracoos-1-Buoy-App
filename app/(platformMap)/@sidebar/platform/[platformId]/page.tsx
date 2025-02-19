290fc08a7ddea59fad4333a450a04566e63b04a1
Alex Kerney <abk@mac.com>
2025-02-19 13:41:00 -0500

    Doesn't want to use use

--- app/(platformMap)/@sidebar/platform/[platformId]/page.tsx
+++ app/(platformMap)/@sidebar/platform/[platformId]/page.tsx
@@ -1,10 +1,11 @@
 import { PlatformInfo } from "components/PlatformInfo/platformInfo"
-import React, { use } from "react"
+import React from "react"
+
 import { useDecodedUrl } from "util/hooks"
 
-export default function PlatformSidebar(props: { params: Promise<{ platformId: string }> }) {
-  const params = use(props.params)
-  const id = useDecodedUrl(params.platformId)
+export default function PlatformSidebar(props: { params: { platformId: string } }) {
+  const {platformId} = props.params
+  const id = useDecodedUrl(platformId)
 
   return <PlatformInfo id={id} />
 }
