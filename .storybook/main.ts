0d84805847d472cfaf723b8fa26e4a9952369e8f
Alex Kerney <abk@mac.com>
2025-09-08 16:33:08 -0400

    Update Storybook 9.1.5
    Update Storybook from 8.6.12 to 9.1.5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,13 +3,7 @@ import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.[jt]s", "../src/**/*.stories.[jt]sx", "../app/**/*.stories.[jt]sx"],
 
-  addons: [
-    "@storybook/addon-a11y",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    // "@chromatic-com/storybook"
-  ],
+  addons: ["@storybook/addon-a11y", "@storybook/addon-links", "@storybook/addon-docs"],
 
   framework: "@storybook/nextjs",
 
