73e33280a15226e63c39c5a7b46362a719282aa8
cgalvarino <charlton@2creek.com>
2026-03-17 16:27:40 +0000

    Adds simple previous and next region links based on the regionList order.

--- app/(platformMap)/@sidebar/region/[regionId]/region.tsx
+++ app/(platformMap)/@sidebar/region/[regionId]/region.tsx
@@ -1,7 +1,36 @@
 "use client"
 import { ErddapPlatformList } from "Features/ERDDAP"
-import { Region } from "Shared/regions"
+import { Region, regionList } from "Shared/regions"
+import { paths } from "Shared/constants"
+import { urlPartReplacer } from "Shared/urlParams"
+
+import Link from "next/link"
 
 export function RegionList({ region }: { region: Region }) {
   return <ErddapPlatformList boundingBox={region.bbox} />
+}
+
+export function NextRegion({ region, offset }: { region: Region; offset: number }) {
+  let regionIdx = regionList.findIndex((r) => r.slug === region.slug)
+  if (regionIdx < 0) {
+    return <div />
+  }
+
+  let nextRegionIdx = (regionIdx + regionList.length + offset) % regionList.length
+  let prevRegionIdx = (regionIdx + regionList.length - 1) % regionList.length
+  // Don't show if we only have 1 region or we're trying to show next and it's the same as prev.
+  if (regionIdx == nextRegionIdx || (offset == 1 && nextRegionIdx == prevRegionIdx)) {
+    return <div />
+  }
+
+  return (
+    <div>
+      <Link
+        href={urlPartReplacer(paths.regions.region, ":id", regionList[nextRegionIdx].slug as string)}
+        key={regionList[nextRegionIdx].name}
+      >
+        {regionList[nextRegionIdx].name}
+      </Link>
+    </div>
+  )
 }
