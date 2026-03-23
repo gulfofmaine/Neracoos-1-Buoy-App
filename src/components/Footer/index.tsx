1587189bff6b649b8446b9f67a13370fbf64211f
shindelr <129821483+shindelr@users.noreply.github.com>
2026-03-23 13:23:18 -0400

    Top level layout & footer inlines converted

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,27 +3,18 @@ import * as React from "react"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 
-const textStyle = { fontSize: ".8rem", padding: ".25rem" }
-
 const colSize = 10
 
 export const Footer: React.FunctionComponent = () => (
   <div
-    className="footer"
-    style={{
-      position: "absolute",
-      bottom: 0,
-      width: "100%",
-      paddingBottom: "10px",
-      height: "110px",
-    }}
+    className="mt-auto"
   >
-    <Row style={{ ...textStyle, paddingTop: "1rem" }}>
+    <Row>
       <Col md={colSize} className="mx-auto text-center">
         Copyright © 2021 ~ NERACOOS
       </Col>
     </Row>
-    <Row style={textStyle}>
+    <Row>
       <Col md={colSize} className="mx-auto text-center">
         Use{" "}
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdzP90655d-ZuDGpdgcMmTvxW1sgR_Hg4KI1KCfQDFU8MMF0g/viewform?usp=sf_link">
@@ -32,7 +23,7 @@ export const Footer: React.FunctionComponent = () => (
         to send us feedback or bug reports
       </Col>
     </Row>
-    <Row style={textStyle}>
+    <Row>
       <Col md={colSize} className="mx-auto text-center">
         Product of <a href="http://www.neracoos.org/">NERACOOS.org</a> - Developed and maintained by the{" "}
         <a href="https://gmri.org/commitments/science/ocean-data-products/">Gulf of Maine Research Institute</a>
