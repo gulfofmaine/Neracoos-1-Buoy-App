15dbda7d9463220aa67c028429017a1f004dc011
shindelr <129821483+shindelr@users.noreply.github.com>
2026-03-30 15:24:14 -0400

    linter

--- src/components/SecondaryBanner/index.tsx
+++ src/components/SecondaryBanner/index.tsx
@@ -1,5 +1,9 @@
 import Alert from "react-bootstrap/Alert"
 
 export const SecondaryBanner = ({ children }) => {
-  return <Alert className="secondary-alert d-flex justify-content-center p-1 px-2 px-md-1 rounded-0 min-wh-100">{children}</Alert>
+  return (
+    <Alert className="secondary-alert d-flex justify-content-center p-1 px-2 px-md-1 rounded-0 min-wh-100">
+      {children}
+    </Alert>
+  )
 }
