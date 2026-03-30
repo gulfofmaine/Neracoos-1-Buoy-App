22f58dd2969771e6efb66379f2832d557f45f6a5
shindelr <129821483+shindelr@users.noreply.github.com>
2026-03-30 17:53:45 -0400

    Footer styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,31 +3,23 @@ import * as React from "react"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 
-const colSize = 10
-
 export const Footer: React.FunctionComponent = () => (
-  <div className="mt-auto">
-    <Row className="pt-1">
-      <Col md={colSize} className="mx-auto text-center">
-        Copyright © 2021 ~ NERACOOS
-      </Col>
-    </Row>
-    <Row>
-      <Col md={colSize} className="mx-auto text-center">
+  <footer className="mt-auto caption py-6 bg-secondary bg-opacity-5">
+    <Row className="text-center">
+      <Col xs={12} md={12}>{`Copyright © ${new Date().getFullYear()} - NERACOOS`}</Col>
+      <Col xs={12} md={12}>
         Use{" "}
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdzP90655d-ZuDGpdgcMmTvxW1sgR_Hg4KI1KCfQDFU8MMF0g/viewform?usp=sf_link">
           this form
         </a>{" "}
         to send us feedback or bug reports
       </Col>
-    </Row>
-    <Row>
-      <Col md={colSize} className="mx-auto text-center">
+      <Col xs={12} md={12}>
         Product of <a href="http://www.neracoos.org/">NERACOOS.org</a> - Developed and maintained by the{" "}
         <a href="https://gmri.org/commitments/science/ocean-data-products/">Gulf of Maine Research Institute</a>
       </Col>
     </Row>
-  </div>
+  </footer>
 )
 
 export default Footer
