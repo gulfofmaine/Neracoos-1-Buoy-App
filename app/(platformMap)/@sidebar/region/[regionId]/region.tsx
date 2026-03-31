26d4914e9d20cf79cce751b15bd5318131e273f5
shindelr <129821483+shindelr@users.noreply.github.com>
2026-03-31 16:36:30 -0400

    Fixed icon prop to allow size, fixed icon color

--- app/(platformMap)/@sidebar/region/[regionId]/region.tsx
+++ app/(platformMap)/@sidebar/region/[regionId]/region.tsx
@@ -31,9 +31,9 @@ export function NextRegion({ region, offset }: { region: Region; offset: number 
         key={regionList[nextRegionIdx].name}
         className="d-flex flex-row align-items-center gap-1"
       >
-        {offset < 0 && <ArrowLeftIcon height={14} className="" />}
+        {offset < 0 && <ArrowLeftIcon height={14} className="text-primary" />}
         <span className="text-center">{regionList[nextRegionIdx].name}</span>
-        {offset > 0 && <ArrowRightIcon height={14} className="" />}
+        {offset > 0 && <ArrowRightIcon height={14} className="text-primary" />}
       </Link>
     </>
   )
--- src/Shared/icons/iconComponent.tsx
+++ src/Shared/icons/iconComponent.tsx
@@ -7,11 +7,12 @@ import { IconDefinition } from "@fortawesome/fontawesome-svg-core"
 
 type IconProps = {
   className?: string
+  height?: number
 }
 
 function mkIcon(faIcon: IconDefinition) {
-  function Icon({ className, ...props }: IconProps) {
-    return <FontAwesomeIcon icon={faIcon} className={className} {...props} />
+  function Icon({ className, height, ...rest }: IconProps) {
+    return <FontAwesomeIcon icon={faIcon} className={className} height={height} {...rest} />
   }
   return Icon
 }
