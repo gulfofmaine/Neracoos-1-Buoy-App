485f0e1ef9d8f54aaa8a4da091a40e900abe046b
shindelr <129821483+shindelr@users.noreply.github.com>
2026-04-09 17:52:00 -0400

    Scrollable station list on region view, map height same as sidebar height

--- app/(platformMap)/layout.tsx
+++ app/(platformMap)/layout.tsx
@@ -30,10 +30,6 @@ export default function Layout({
     setIsClient(true)
   }, [])
 
-  if (height < 420) {
-    height = 420
-  }
-
   //Removing warning in console re:Highcharts library defaultProps. Not an active warning for us--until this issues is solved on Highcharts' end, keep this to remove huge console error
   const error = console.error
   console.error = (...args: any) => {
@@ -45,11 +41,15 @@ export default function Layout({
     <React.Fragment>
       <Row className="g-5">
         <Col xs={12} md={6} className="order-2">
-          <div ref={ref}>{sidebar}</div>
+          <div className="platform-map-layout" ref={ref}>
+            {sidebar}
+          </div>
         </Col>
 
         <Col xs={12} md={6} className="order-1">
-          <ErddapMap height={params.regionId ? "80vh" : height} {...(isPlatformView && { platformId })} />
+          <div className="platform-map-layout">
+            <ErddapMap className="map" {...(isPlatformView && { platformId })} />
+          </div>
           {belowMap ?? <React.Fragment>{belowMap}</React.Fragment>}
         </Col>
       </Row>
