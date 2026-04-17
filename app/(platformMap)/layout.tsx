f8588c83bc8d7f1138009a7951a26ec0c81261c7
shindelr <129821483+shindelr@users.noreply.github.com>
2026-04-17 09:22:46 -0700

    Added some comments for clarification

--- app/(platformMap)/layout.tsx
+++ app/(platformMap)/layout.tsx
@@ -41,6 +41,7 @@ export default function Layout({
       <Row className="g-5 align-items-stretch">
         <Col xs={12} md={6} className="order-2 d-flex">
           <div className="platform-map-layout flex-fill">
+            {/* Second div layer decouples sidebar from map to allow for scrolling */}
             <div className={isRegionView ? "h-60" : ""}>{sidebar}</div>
           </div>
         </Col>
@@ -48,6 +49,7 @@ export default function Layout({
         <Col xs={12} md={6} className="order-1 d-flex flex-column">
           <div className="platform-map-layout flex-fill">
             <ErddapMap
+              // Pass minimum height requirement only on landing page
               className={`${!(isPlatformView || isRegionView) ? "landing-min-height" : ""} map`}
               {...(isPlatformView && { platformId })}
             />
