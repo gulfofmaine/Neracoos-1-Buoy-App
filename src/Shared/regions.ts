7cb5a83343911a764be37c71ea4107b63dfd166f
cgalvarino <charlton@2creek.com>
2026-05-20 23:35:36 +0000

    linter love

--- src/Shared/regions.ts
+++ src/Shared/regions.ts
@@ -27,135 +27,135 @@ export const InitialRegion: Region = {
 }
 
 const DowneastMaine: Region = {
-    bbox: {
-      east: -66.0307,
-      north: 45.2496,
-      south: 42.9925,
-      west: -68.5832,
-    },
-   name: "Downeast Maine",
-   slug: "DOWNEAST",
+  bbox: {
+    east: -66.0307,
+    north: 45.2496,
+    south: 42.9925,
+    west: -68.5832,
+  },
+  name: "Downeast Maine",
+  slug: "DOWNEAST",
 }
 
 const PenobscotBay: Region = {
-    bbox: {
-      east: -67.9132,
-      north: 44.7921,
-      south: 42.9798,
-      west: -69.4501,
-    },
-   name: "Penobscot Bay",
-   slug: "PENBAY",
+  bbox: {
+    east: -67.9132,
+    north: 44.7921,
+    south: 42.9798,
+    west: -69.4501,
+  },
+  name: "Penobscot Bay",
+  slug: "PENBAY",
 }
 
 const MidcoastMaine: Region = {
-    bbox: {
-      east: -69.1218,
-      north: 44.1782,
-      south: 43.0892,
-      west: -70.0485,
-    },
-   name: "Midcoast Maine",
-   slug: "MIDCOAST",
+  bbox: {
+    east: -69.1218,
+    north: 44.1782,
+    south: 43.0892,
+    west: -70.0485,
+  },
+  name: "Midcoast Maine",
+  slug: "MIDCOAST",
 }
 
 const SouthernMaine: Region = {
-    bbox: {
-      east: -69.4900,
-      north: 44.2859,
-      south: 43.0702,
-      west: -70.7603,
-    },
-   name: "Southern Maine Coast",
-   slug: "SOUTHME",
+  bbox: {
+    east: -69.4900,
+    north: 44.2859,
+    south: 43.0702,
+    west: -70.7603,
+  },
+  name: "Southern Maine Coast",
+  slug: "SOUTHME",
 }
 
 const SeacoastNH: Region = {
-    bbox: {
-      east: -70.3676,
-      north: 43.2703,
-      south: 42.6959,
-      west: -71.0204,
-    },
-   name: "Seacoast NH",
-   slug: "SEACOAST",
+  bbox: {
+    east: -70.3676,
+    north: 43.2703,
+    south: 42.6959,
+    west: -71.0204,
+  },
+  name: "Seacoast NH",
+  slug: "SEACOAST",
 }
 
 const MassBay: Region = {
-    bbox: {
-      east: -70.0190,
-      north: 42.6618,
-      south: 41.6029,
-      west: -71.1897,
-    },
-   name: "Massachusetts Bay",
-   slug: "MASSBAY",
+  bbox: {
+    east: -70.0190,
+    north: 42.6618,
+    south: 41.6029,
+    west: -71.1897,
+  },
+  name: "Massachusetts Bay",
+  slug: "MASSBAY",
 }
 
 const OffshoreGOM: Region = {
-    bbox: {
-      east: -64.1990,
-      north: 44.4165,
-      south: 39.1620,
-      west: -70.4649,
-    },
-   name: "Offshore Gulf of Maine",
-   slug: "OFFSHOREGOM",
+  bbox: {
+    east: -64.1990,
+    north: 44.4165,
+    south: 39.1620,
+    west: -70.4649,
+  },
+  name: "Offshore Gulf of Maine",
+  slug: "OFFSHOREGOM",
 }
 
 const SoutheastMA: Region = {
-    bbox: {
-      east: -69.3886,
-      north: 42.3492,
-      south: 40.8326,
-      west: -71.1430,
-    },
-   name: "Southeast Massachusetts",
-   slug: "SOUTHEASTMA",
+  bbox: {
+    east: -69.3886,
+    north: 42.3492,
+    south: 40.8326,
+    west: -71.1430,
+  },
+  name: "Southeast Massachusetts",
+  slug: "SOUTHEASTMA",
 }
 
 const NarraBay: Region = {
-    bbox: {
-      east: -70.9573,
-      north: 41.9614,
-      south: 41.3087,
-      west: -71.6696,
-    },
-   name: "Narragansett Bay",
-   slug: "NARRBAY",
+  bbox: {
+    east: -70.9573,
+    north: 41.9614,
+    south: 41.3087,
+    west: -71.6696,
+  },
+  name: "Narragansett Bay",
+  slug: "NARRBAY",
 }
 
 const RISound: Region = {
-    bbox: {
-      east: -70.5495,
-      north: 41.5160,
-      south: 40.5382,
-      west: -71.6805,
-    },
-   name: "Rhode Island Sound",
-   slug: "RISOUND",
+  bbox: {
+    east: -70.5495,
+    north: 41.5160,
+    south: 40.5382,
+    west: -71.6805,
+  },
+  name: "Rhode Island Sound",
+  slug: "RISOUND",
 }
 
 const LISound: Region = {
-    bbox: {
-      east: -72.0031,
-      north: 41.7588,
-      south: 40.1544,
-      west: -74.0863,
-    },
-   name: "Long Island Sound",
-   slug: "LISOUND",
+  bbox: {
+    east: -72.0031,
+    north: 41.7588,
+    south: 40.1544,
+    west: -74.0863,
+  },
+  name: "Long Island Sound",
+  slug: "LISOUND",
 }
 
 const NYBight: Region = {
-    bbox: {
-      east: -71.3516,
-      north: 40.9116,
-      south: 38.5304,
-      west: -74.3516,
-    },
-   name: "New York Bight",
-   slug: "NYBIGHT",
+  bbox: {
+    east: -71.3516,
+    north: 40.9116,
+    south: 38.5304,
+    west: -74.3516,
+  },
+  name: "New York Bight",
+  slug: "NYBIGHT",
 }
 
 export const regions = {
