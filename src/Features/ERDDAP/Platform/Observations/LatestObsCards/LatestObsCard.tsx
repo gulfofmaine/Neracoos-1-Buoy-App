e2b5885c8a4dd1cf966791656f9bdbba2eb12b8f
Robin Shindelman <129821483+shindelr@users.noreply.github.com>
2026-05-26 11:34:59 -0400

    Add wrap to primary data (#4128)

--- src/Features/ERDDAP/Platform/Observations/LatestObsCards/LatestObsCard.tsx
+++ src/Features/ERDDAP/Platform/Observations/LatestObsCards/LatestObsCard.tsx
@@ -64,7 +64,7 @@ export const LatestObsCard = ({ unitSystem, timeSeries, platform }: CardDisplayP
               <p className="text-black-65 mb-0">{groupName}</p>
 
               {/* Primary value and unit */}
-              <span className="d-flex flex-row align-items-end">
+              <span className="d-flex flex-row flex-wrap align-items-end">
                 <h1 className="mb-0">{cardData.primary}</h1>
                 <p className="text-black-65 ms-1 m-0">{cardData.primaryUnit}</p>
               </span>
